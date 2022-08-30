d70668d980b202fb297ea7d85ab562578bb58865
JITENDRA KUMAR RANA <guddujitendrarana@gmail.com>
2022-08-31 01:55:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import streamlit as st
 celsius = st.number_input('Insert a number')
-st.write('The current number is ',celsius)
 def compute(celsius):
     #coverting c to f
     c = int(celsius)
     f = (c * 1.8) + 32
     st.dataframe(f)
+    st.write('The current number is ',f)
     #print("fahrenheit : " , fahrenheit ,"F")
     #f = st.number_input('celsius')
 #     st.write('The Temperature in (F): ', f)
