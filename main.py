e66f0ade888413313fbdac2cb1dc173416353c02
JITENDRA KUMAR RANA <guddujitendrarana@gmail.com>
2022-08-30 21:16:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 def compute(celsius):
     #coverting c to f
-    fahrenheit = (celsius * 1.8) + 32
+    c = int(celsius)
+    fahrenheit = (c * 1.8) + 32
     #print("fahrenheit : " , fahrenheit ,"F")
     return fahrenheit
-# c=input("Enter the temperature in celsius(C): ")
-# a=int(c)
-# compute(a)
+compute(input("Enter the temperature in celsius(C): "))
+
