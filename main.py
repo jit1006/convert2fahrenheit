87880d5f6499ca2e27589a2a4fdff045f7af746e
JITENDRA KUMAR RANA <guddujitendrarana@gmail.com>
2022-08-31 22:13:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ d = int(f)
 st.header("Display the metric")
 
 col1, col2, col3 = st.columns(3)
-col1.metric("Temperature", d + " °F", "1.2 °F")
+col1.metric("Temperature", d , " °F", "1.2 °F")
 col2.metric("Wind", "9 mph", "-8%")
 col3.metric("Humidity", "86%", "4%")
 
