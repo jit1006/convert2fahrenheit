2cdefa881f749a5f6f6b158bed8050b5c4d846cc
JITENDRA KUMAR RANA <guddujitendrarana@gmail.com>
2022-08-31 22:09:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ c = st.number_input('Enter the celsius temperature ')
 
 #coverting c to f
 f = (c * 1.8) + 32
-# d = float(f)
-st.write('The Fahrenheit temperature is ', f )
 
+st.write('The Fahrenheit temperature is ', f )
+d = int(f)
 st.header("Display the metric")
 
 col1, col2, col3 = st.columns(3)
-col1.metric("Temperature", f ,"°F", "1.2 °F")
+col1.metric("Temperature", d ,"°F")
 col2.metric("Wind", "9 mph", "-8%")
 col3.metric("Humidity", "86%", "4%")
 
