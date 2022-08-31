f415fa5f622c9216512126cf089210c838572526
JITENDRA KUMAR RANA <guddujitendrarana@gmail.com>
2022-08-31 21:42:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ c = st.number_input('Enter the celsius temperature ')
 
 #coverting c to f
 f = (c * 1.8) + 32
-# d = float(f)
-st.write('The Fahrenheit temperature is ', f )
-print("fahrenheit : " , f ,"F")
+d = float(f)
+st.write('The Fahrenheit temperature is ', d )
+
