f2583dfcba706e4cb1891505e01fb6e24d73c9a4
JITENDRA KUMAR RANA <guddujitendrarana@gmail.com>
2022-08-31 21:30:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import streamlit as st
+
+celsius = st.number_input('Enter the celsius temperature ')
 def compute(celsius):
     #coverting c to f
     f = (c * 1.8) + 32
-    return f
+    d = float(f)
+    st.write('The Fahrenheit temperature is ', d )
+#     return f
     #print("fahrenheit : " , fahrenheit ,"F")
     # compute(input("Enter the temperature in celsius(C): "))
 
